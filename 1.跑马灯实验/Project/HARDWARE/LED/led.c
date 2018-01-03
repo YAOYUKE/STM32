df8b9bf12027e14eb4d00c8ca6a3148c2017e243
姚宇科 <766241111@qq.com>
2018-01-03 19:36:47 +0800

    NONE

--- 1.跑马灯实验/Project/HARDWARE/LED/led.c
+++ 1.跑马灯实验/Project/HARDWARE/LED/led.c
@@ -20,17 +20,17 @@ void LED_Init(void)
 	/* 配置IO口 A */
 	GPIO_InitTypeDef GPIO_InitStructure;                     // 指向GPIO InitTypeDef结构的指针，该结构包含指定的GPIO外围设备的配置信息
 	
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         // 推挽输出
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;                // 配置IO口针脚 8
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;        // 速度：10MHz
-	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         // 推挽输出
+
 	GPIO_Init(GPIOA,&GPIO_InitStructure);                    // 初始化GPIOA.8配置信息：推挽输出模式；速度50MHz
 	
     /* 配置IO口 D */
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         // 推挽输出
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;                // 配置IO口针脚 2
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;        // 速度：10MHz
-	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         // 推挽输出
+
 	GPIO_Init(GPIOD,&GPIO_InitStructure);                    // 初始化GPIOD.2配置信息：推挽输出模式；速度50MHz
 	
 	
